0ab6f69c018bd5a279b42f6b87c6d48443449633
Bianca <bianca.costamelo1@gmail.com>
2025-01-18 09:52:06 -0300

    adicionando função javascript

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -1,1 +1,20 @@
+let btns = document.querySelectorAll('.btnValue')
+let result = document.querySelector('.result-test')
+let value = null
+const card1 = document.getElementById('card1')
+
+btns.forEach((e) => {
+    e.addEventListener('click', () => {
+        value = e.value
+    })
+})
+
+function enviar() {
+    if (value) {
+        card1.style.display = 'none'
+        result.innerHTML = `You selected <span id="number-select">${value}</span> out of 5`
+    } else {
+        result.innerHTML = `Select a number`
+    }
+}
 

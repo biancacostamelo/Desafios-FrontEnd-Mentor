b189d237f9396254c6c0e979076eb543e838a65d
Bianca <bianca.costamelo1@gmail.com>
2025-01-20 13:24:11 -0300

    finalizando projeto

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -2,6 +2,7 @@ let btns = document.querySelectorAll('.btnValue')
 let result = document.querySelector('.result-test')
 let value = null
 const card1 = document.getElementById('card1')
+const card2 = document.getElementById('card2')
 
 btns.forEach((e) => {
     e.addEventListener('click', () => {
@@ -12,9 +13,15 @@ btns.forEach((e) => {
 function enviar() {
     if (value) {
         card1.style.display = 'none'
+        card2.style.display = 'flex'
         result.innerHTML = `You selected <span id="number-select">${value}</span> out of 5`
     } else {
         result.innerHTML = `Select a number`
     }
+}
+
+function voltar(){
+    card1.style.display = 'flex'
+    card2.style.display = 'none'
 }
 
